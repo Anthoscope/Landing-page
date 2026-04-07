72b4568da7c1fab0d387f2edbcdf96fafa233ebf
Epatsatzaki <evapatsatzakis@gmail.com>
2026-04-07 22:05:05 +0300

    testt

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This tells Vite to look for assets in the CURRENT folder
+  base: '/Anthoscope-Landing-page/', 
 })
